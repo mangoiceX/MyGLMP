ad1106f72121d95b6b00b18d2b618ec2ecbfb26c
mango <2075419247@qq.com>
2020-12-18 11:37:33 +0800

    编写read_file函数，并调试

--- data_loader/data_Ent_babi.py
+++ data_loader/data_Ent_babi.py
@@ -28,50 +28,55 @@ def getEntityList(type_dict):
 def read_file(file_train, type_dict, entityList):
     ent_word = set()  #统计response的单词
     context_arr,data = [],[]
+    max_resp_len = 0
     with open(file_train,'r') as f:
         for line in f.readlines():
             line = line.strip('\n')
-            turn_id, line = line.split(' ',1)
-            if '\t' in line:
-                user_utterance, response = line.split('\t')
-                #利用用户的对话生成四元组(word,$u,turn_id,word_id)
-                gen_u = generate_memory(user_utterance,'$u',turn_id)
-                context_arr += gen_u
-                #计算local pointer的标准值
-                local_ptr = []
-                for key in response.split(' '):
-                    ent_word.add(key)
-                    if key in entityList:
-                        for i in range(len(context_arr), -1):
-                            if context_arr[i][0] == key:
-                                local_ptr.append(i)
-                                break
-                    else:
-                        local_ptr.append(len(context_arr))
-                #计算全局指针，统计上下文有没有在当前回答中出现
-                global_ptr =[1 if triplet[0] in response.split() else 0 for triplet in context_arr]
-                sketch_response = generate_sketch_response(response,entityList,type_dict)
+            if line:
+                turn_id, line = line.split(' ',1)
+                if '\t' in line:
+                    user_utterance, response = line.split('\t')
+                    #利用用户的对话生成四元组(word,$u,turn_id,word_id)
+                    gen_u = generate_memory(user_utterance,'$u',turn_id)
+                    context_arr += gen_u
+                    #计算local pointer的标准值
+                    local_ptr = []
+                    for key in response.split(' '):
+                        ent_word.add(key)
+                        if key in entityList:
+                            for i in range(len(context_arr), -1):
+                                if context_arr[i][0] == key:
+                                    local_ptr.append(i)
+                                    break
+                        else:
+                            local_ptr.append(len(context_arr))
+                    #计算全局指针，统计上下文有没有在当前回答中出现
+                    global_ptr =[1 if triplet[0] in response.split() else 0 for triplet in context_arr]
+                    sketch_response = generate_sketch_response(response,entityList,type_dict)
 
-                gen_r = generate_memory(response, '$r', turn_id)
-                context_arr += gen_r #将当前回答加入上下文
-                data_details = {
-                    'context_arr':context_arr +[['$$$']*MEM_TOKEN_SIZE], #为什么后面要追加NULL表示符号？
-                    'response':response,
-                    "local_ptr":local_ptr,
-                    'global_ptr':global_ptr,
-                    'sketch_response':sketch_response
-                }
-                data.append(data_details)
-            else: #表示这段话结束
+                    gen_r = generate_memory(response, '$r', turn_id)
+                    context_arr += gen_r #将当前回答加入上下文
+                    data_details = {
+                        'context_arr':context_arr +[['$$$']*MEM_TOKEN_SIZE], #为什么后面要追加NULL表示符号？
+                        'response':response,
+                        "local_ptr":local_ptr,
+                        'global_ptr':global_ptr,
+                        'sketch_response':sketch_response
+                    }
+                    data.append(data_details)
+                    max_resp_len = max(max_resp_len,len(response.split()))  #记录回答的最长长度
+                else: #表示这段话结束
+                    context_arr = []  #这里不知道是做什么
+            else:
                 context_arr = []
 
-    return data
+    return data, max_resp_len
 
 
 def generate_sketch_response(response,global_entity,type_dict):
     #对回答的每个单词生成@type的描述
     sketch_response = []
-    for word in response:
+    for word in response.split():
         if word in global_entity:
             entity_type = None
             for key in type_dict.keys():
@@ -89,13 +94,10 @@ def generate_memory(tokens,speaker,turn_id):
     gen_content = []
     if speaker == '$u' or speaker == '$r':
         for idx, token in enumerate(tokens):
-            tmp = [token, speaker, 'turn'+str(turn_id),'word'+str(idx)] + ['PAD']*(MEM_TOKEN_SIZE-4)
+            tmp = [token, speaker, 'turn'+turn_id,'word'+str(idx)] + ['PAD']*(MEM_TOKEN_SIZE-4)
             gen_content.append(tmp)
 
     return gen_content
-
-
-
 
 
 
@@ -116,6 +118,11 @@ def prepare_data(task):
     #实体列表
     entityList = getEntityList(type_dict)
 
-    train_data = read_file(file_train, type_dict, entityList)
+    train_data,max_trn_len = read_file(file_train, type_dict, entityList)
+    dev_data,max_dev_len = read_file(file_dev, type_dict, entityList)
+    tst_data,max_tst_len = read_file(file_tst, type_dict, entityList)
+    tst_oov_data,max_tst_oov_len = read_file(file_tst_oov, type_dict,entityList)
+    print(train_data[:4])
+
     # print(type_dict.keys())
 prepare_data(1)
--- utils/config.py
+++ utils/config.py
@@ -2,7 +2,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description = "parameters")
 parser.add_argument('dataset')
-args = parser.parse_args()
+args = vars(parser.parse_args())
 
 
 MEM_TOKEN_SIZE = 6 if args["dataset"] == 'kvr' else 4
